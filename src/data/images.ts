5cee31decc17cbd87dac36e86f7d98f4dbe5f1ee
joacnt <constabeljoaquinn@gmail.com>
2025-02-21 14:12:33 -0300

    Create Web

---
+++ src/data/images.ts
@@ -0,0 +1,7 @@
+export const images = [
+    { url: '/images/slider-1.webp', alt: 'Imagen de prueba 1' },
+    { url: '/images/slider-2.webp', alt: 'Imagen de prueba 2' },
+    { url: '/images/slider-3.webp', alt: 'Imagen de prueba 3' },
+    { url: '/images/slider-4.webp', alt: 'Imagen de prueba 4' },
+  ];
+  
