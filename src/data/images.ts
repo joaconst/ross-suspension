36ab06c9b0a611189357eb96e4a939370ac1b957
joacnt <constabeljoaquinn@gmail.com>
2025-02-21 22:35:35 -0300

    Responsive components

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,13 +1,16 @@
 export const desktopImages = [
-  { url: '/images/slider-1.webp', alt: 'Descripción desktop 1' },
-  { url: '/images/slider-2.webp', alt: 'Descripción desktop 2' },
-  { url: '/images/slider-3.webp', alt: 'Descripción desktop 3' },
-  { url: '/images/slider-4.webp', alt: 'Descripción desktop 4' },
+  { url: '/images/slider-desktop/slider-1.webp', alt: 'Descripción desktop 1' },
+  { url: '/images/slider-desktop/slider-2.webp', alt: 'Descripción desktop 2' },
+  { url: '/images/slider-desktop/slider-3.webp', alt: 'Descripción desktop 3' },
+  { url: '/images/slider-desktop/slider-4.webp', alt: 'Descripción desktop 4' },
 ];
 
 export const mobileImages = [
-  { url: '/images/slider-1.webp', alt: 'Descripción desktop 1' },
-  { url: '/images/slider-2.webp', alt: 'Descripción desktop 2' },
-  { url: '/images/slider-3.webp', alt: 'Descripción desktop 3' },
-  { url: '/images/slider-4.webp', alt: 'Descripción desktop 4' },
+  { url: '/images/slider-mobile/slider-mobile-1.webp', alt: 'Descripción mobile 1' },
+  { url: '/images/slider-mobile/slider-mobile-2.webp', alt: 'Descripción mobile 2' },
+  { url: '/images/slider-mobile/slider-mobile-3.webp', alt: 'Descripción mobile 3' },
+  { url: '/images/slider-mobile/slider-mobile-4.webp', alt: 'Descripción mobile 4' },
+  { url: '/images/slider-mobile/slider-mobile-5.webp', alt: 'Descripción mobile 5' },
+  { url: '/images/slider-mobile/slider-mobile-6.webp', alt: 'Descripción mobile 6' },
+  { url: '/images/slider-mobile/slider-mobile-7.webp', alt: 'Descripción mobile 7' },
 ];
