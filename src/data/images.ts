9b491d029a256bde3fbb4666d30f38c2dbe0e99b
joacnt <constabeljoaquinn@gmail.com>
2025-02-23 23:27:51 -0300

    Changes extras

--- src/data/images.ts
+++ src/data/images.ts
@@ -6,23 +6,17 @@ export const desktopImages = [
 ];
 
 export const mobileImages = [
-  { url: '/images/slider-mobile/slider-mobile-1.webp', alt: 'Descripción mobile 1' },
-  { url: '/images/slider-mobile/slider-mobile-2.webp', alt: 'Descripción mobile 2' },
-  { url: '/images/slider-mobile/slider-mobile-3.webp', alt: 'Descripción mobile 3' },
-  { url: '/images/slider-mobile/slider-mobile-4.webp', alt: 'Descripción mobile 4' },
-  { url: '/images/slider-mobile/slider-mobile-5.webp', alt: 'Descripción mobile 5' },
+  { url: '/images/slider-mobile/top/slider-mobile-1.webp', alt: 'Descripción mobile 1' },
+  { url: '/images/slider-mobile/top/slider-mobile-2.webp', alt: 'Descripción mobile 2' },
+  { url: '/images/slider-mobile/top/slider-mobile-3.webp', alt: 'Descripción mobile 3' },
+  { url: '/images/slider-mobile/top/slider-mobile-4.webp', alt: 'Descripción mobile 4' },
+  { url: '/images/slider-mobile/top/slider-mobile-5.webp', alt: 'Descripción mobile 5' },
 ];
 
 export const desktopImages2 = [
-  { url: '/images/slider-desktop/bottom/slider-1.webp', alt: 'Descripción desktop 1' },
+  { url: '/images/slider-desktop/bottom/slider-1.webp', alt: 'Descripción desktop 1' }
 ];
 
 export const mobileImages2 = [
-  { url: '/images/slider-mobile/slider-mobile-1.webp', alt: 'Descripción mobile 1' },
-  { url: '/images/slider-mobile/slider-mobile-2.webp', alt: 'Descripción mobile 2' },
-  { url: '/images/slider-mobile/slider-mobile-3.webp', alt: 'Descripción mobile 3' },
-  { url: '/images/slider-mobile/slider-mobile-4.webp', alt: 'Descripción mobile 4' },
-  { url: '/images/slider-mobile/slider-mobile-5.webp', alt: 'Descripción mobile 5' },
-  { url: '/images/slider-mobile/slider-mobile-6.webp', alt: 'Descripción mobile 6' },
-  { url: '/images/slider-mobile/slider-mobile-7.webp', alt: 'Descripción mobile 7' },
+  { url: '/images/slider-mobile/bottom/slider-mobile-1.webp', alt: 'Descripción mobile 1' }
 ];
